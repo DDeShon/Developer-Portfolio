fdd81144708dd0ae711d6e2694c396f66e59ab2d
DDeShon <36036284+DDeShon@users.noreply.github.com>
2021-05-20 06:57:02 -0500

    updated php

--- contactform.php
+++ contactform.php
@@ -1,6 +1,6 @@
 <?php 
 
-
+if(isset($_POST['submit'])) {
     $name = $_POST['name'];
     $company = $_POST['company'];
     $emailFrom = $_POST['email'];
@@ -13,8 +13,8 @@
     $txt = "You have received an email from ".$name.".\n\n".$message;
 
     mail($emailTo, $subject, $txt, $headers);
-
+    
     header("Location: index.html");
-
+}
 
 ?>
